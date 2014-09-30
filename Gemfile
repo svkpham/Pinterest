0d5e260d32e046c6f1d843b7d1aaaf6651b92531
Steven Pham <steven.pham@gmail.com>
2014-09-29 21:28:14 -0700

    Added devise and users

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
+gem 'devise'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #required for Heroku
+  config.action_mailer.default_url_options = { :host => 'http://svkpinterest-commits.herokuapp.com/' }
+
 end
